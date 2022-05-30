db5660575e872664a116ce319f9dc6da0fade3ae
Wiktor Sadowy <wiktor.sadowy@onet.pl>
2022-05-30 16:07:19 +0200

    Finish notebooks

---
+++ prepare_data_equal.py
@@ -0,0 +1,63 @@
+import csv
+
+COLUMNS = [
+    "Mothers Single Years of Age",
+    "Mothers Age Recode 9",
+    "Cigarettes Before Pregnancy",
+    "Cigarettes 1st Trimester",
+    "Cigarettes 2nd Trimester",
+    "Cigarettes 3rd Trimester",
+    "Body Mass Index",
+    "Body Mass Index Recode",
+    "No Risk Factors Reported",
+    "No Infections Reported",
+    "Five Minute APGAR Score",
+    "Five Minute APGAR Recode"
+]
+
+INDEX_APGAR = 10
+GOOD_APGAR_SCORE = 7
+
+
+INDICES = [
+    [74, 76],
+    [78, 79],
+    [252, 254],
+    [254, 256],
+    [256, 258],
+    [258, 260],
+    [282, 286],
+    [286, 287],
+    [336, 337],
+    [352, 353],
+    [443, 445],
+    [445, 446]
+]
+
+INPUT_FILE = 'Nat2020PublicUS.c20210506.r20210812.txt'
+OUTPUT_FILE = 'natality_dataset_equal.csv'
+
+MAX_NO_POSITIVE_READINGS = 50000
+MAX_NO_NEGATIVE_READINGS = 50000
+
+
+if __name__ == '__main__':
+    with open(OUTPUT_FILE, 'w', encoding='utf-8', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(COLUMNS)
+        with open(INPUT_FILE, encoding='utf-8') as f:
+            no_positive_readings = 0
+            no_negative_readings = 0
+            for no_readings, row in enumerate(f):
+                data = [row[index[0]:index[1]] for index in INDICES]
+                if int(data[INDEX_APGAR]) >= GOOD_APGAR_SCORE and no_positive_readings < MAX_NO_POSITIVE_READINGS:
+                    writer.writerow(data)
+                    no_positive_readings += 1
+                elif int(data[INDEX_APGAR]) < GOOD_APGAR_SCORE and no_negative_readings < MAX_NO_NEGATIVE_READINGS:
+                    writer.writerow(data)
+                    no_negative_readings += 1
+                if no_positive_readings == MAX_NO_POSITIVE_READINGS and \
+                        no_negative_readings == MAX_NO_NEGATIVE_READINGS:
+                    print(f'Number of inputs read before gaining data: {no_readings}')
+                    break
+            print(f'Number of data left: {len(f.readlines())}')
