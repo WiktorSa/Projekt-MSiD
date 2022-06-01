63220e96cedf669985277aa53c2089b78c05bba9
Wiktor Sadowy <wiktor.sadowy@onet.pl>
2022-06-01 23:23:31 +0200

    Fix erasing data

--- prepare_data.py
+++ prepare_data.py
@@ -15,7 +15,6 @@ COLUMNS = [
     "Five Minute APGAR Recode"
 ]
 
-
 INDICES = [
     [74, 76],
     [78, 79],
@@ -31,19 +30,51 @@ INDICES = [
     [445, 446]
 ]
 
+MISSING_VALUES = [
+    None,
+    None,
+    '99',
+    '99',
+    '99',
+    '99',
+    '99.9',
+    '9',
+    '9',
+    '9',
+    '99',
+    '5'
+]
+
 INPUT_FILE = 'Nat2020PublicUS.c20210506.r20210812.txt'
 OUTPUT_FILE = 'natality_dataset.csv'
 
 MAX_NO_READINGS = 100000
 
 
-if __name__ == '__main__':
+def is_data_correct(data):
+    for single_data, not_reported_value in zip(data, MISSING_VALUES):
+        if not_reported_value is not None and single_data == not_reported_value:
+            return False
+    return True
+
+
+def main():
     with open(OUTPUT_FILE, 'w', encoding='utf-8', newline='') as file:
         writer = csv.writer(file)
         writer.writerow(COLUMNS)
         with open(INPUT_FILE, encoding='utf-8') as f:
-            for i, row in enumerate(f, 1):
+            no_correct_readings = 0
+            for no_readings, row in enumerate(f, 1):
                 data = [row[index[0]:index[1]] for index in INDICES]
-                writer.writerow(data)
-                if i == MAX_NO_READINGS:
-                    break
+                if is_data_correct(data):
+                    writer.writerow(data)
+                    no_correct_readings += 1
+                    if no_correct_readings == MAX_NO_READINGS:
+                        print(f'Number of inputs read before gaining data: {no_readings}')
+                        break
+
+            print(f'Number of data left: {len(f.readlines())}')
+
+
+if __name__ == '__main__':
+    main()
--- prepare_data_equal.py
+++ prepare_data_equal.py
@@ -34,6 +34,21 @@ INDICES = [
     [445, 446]
 ]
 
+MISSING_VALUES = [
+    None,
+    None,
+    '99',
+    '99',
+    '99',
+    '99',
+    '99.9',
+    '9',
+    '9',
+    '9',
+    '99',
+    '5'
+]
+
 INPUT_FILE = 'Nat2020PublicUS.c20210506.r20210812.txt'
 OUTPUT_FILE = 'natality_dataset_equal.csv'
 
@@ -41,7 +56,14 @@ MAX_NO_POSITIVE_READINGS = 50000
 MAX_NO_NEGATIVE_READINGS = 50000
 
 
-if __name__ == '__main__':
+def is_data_correct(data):
+    for single_data, not_reported_value in zip(data, MISSING_VALUES):
+        if not_reported_value is not None and single_data == not_reported_value:
+            return False
+    return True
+
+
+def main():
     with open(OUTPUT_FILE, 'w', encoding='utf-8', newline='') as file:
         writer = csv.writer(file)
         writer.writerow(COLUMNS)
@@ -50,14 +72,20 @@ if __name__ == '__main__':
             no_negative_readings = 0
             for no_readings, row in enumerate(f):
                 data = [row[index[0]:index[1]] for index in INDICES]
-                if int(data[INDEX_APGAR]) >= GOOD_APGAR_SCORE and no_positive_readings < MAX_NO_POSITIVE_READINGS:
-                    writer.writerow(data)
-                    no_positive_readings += 1
-                elif int(data[INDEX_APGAR]) < GOOD_APGAR_SCORE and no_negative_readings < MAX_NO_NEGATIVE_READINGS:
-                    writer.writerow(data)
-                    no_negative_readings += 1
-                if no_positive_readings == MAX_NO_POSITIVE_READINGS and \
-                        no_negative_readings == MAX_NO_NEGATIVE_READINGS:
-                    print(f'Number of inputs read before gaining data: {no_readings}')
-                    break
+                if is_data_correct(data):
+                    if int(data[INDEX_APGAR]) >= GOOD_APGAR_SCORE and no_positive_readings < MAX_NO_POSITIVE_READINGS:
+                        writer.writerow(data)
+                        no_positive_readings += 1
+                    elif int(data[INDEX_APGAR]) < GOOD_APGAR_SCORE and no_negative_readings < MAX_NO_NEGATIVE_READINGS:
+                        writer.writerow(data)
+                        no_negative_readings += 1
+                    if no_positive_readings == MAX_NO_POSITIVE_READINGS and \
+                            no_negative_readings == MAX_NO_NEGATIVE_READINGS:
+                        print(f'Number of inputs read before gaining data: {no_readings}')
+                        break
+
             print(f'Number of data left: {len(f.readlines())}')
+
+
+if __name__ == '__main__':
+    main()
